503378c3bef18647377b280e80b711429e501a14
kitaindia <morisot64@gmail.com>
2012-11-24 04:11:26 +0900

    devise修正

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :twitter,"6gyyuefh3GcjZ2Mudkp3ww","9wQExdAjfpYtVFpv0zB9Cj9OParOzEJow56vTBM"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ GeekHouseGcNotifier::Application.routes.draw do
     root :to => 'home#index'
   end
   root :to => "home#index"
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :user do
+    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
   resources :users
 end
