65b9a73ef48de1c0f4bf0a1b06d0d5111d67bc0c
kitaindia <morisot64@gmail.com>
2012-11-23 21:55:18 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeekHouseGcNotifier::Application.config.secret_token = 'aaba42dffe890c2d4af57cf005ba15bb4c5708784e211d28ba8309e3ee45c92917f0d3a75acbe2d865e8e9cb84c4eb58d09236fd0b80d754f56652b96464672d'
