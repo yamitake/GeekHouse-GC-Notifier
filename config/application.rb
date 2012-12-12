58a8bd496151855e4343f3a356307cac6e7dfce8
kitaindia <morisot64@gmail.com>
2012-12-12 14:26:28 +0900

    reconfig unicorn

--- config/application.rb
+++ config/application.rb
@@ -72,5 +72,7 @@ module GeekHouseGcNotifier
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.logger = Logger.new(STDOUT)
   end
 end
