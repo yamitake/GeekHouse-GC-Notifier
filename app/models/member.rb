d3146cbca95962f30e1e09c46edebabaf6272949
kitaindia <morisot64@gmail.com>
2012-12-15 18:58:40 +0900

    validation設定

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,6 @@
 class Member < ActiveRecord::Base
   attr_accessible :image, :name, :turn, :user_id
   belongs_to :user
+  validates :name, length: { maximum: 15 }
+  validates :turn, presence: true, if: :name.blank?
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,10 @@
 class Tweet < ActiveRecord::Base
   attr_accessible :cron_week, :cron_number_week, :message, :user_id
   belongs_to :user
+
+  validates :message,          length: { maximum: 140 }
+  validates :cron_week,        numericality: {greater_than_or_equal_to: 0,
+                                              less_than_or_equal_to: 6}
+  validates :cron_number_week, numericality: {greater_than_or_equal_to: 1,
+                                              less_than_or_equal_to: 4}
 end
